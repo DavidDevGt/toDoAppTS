8559ff354b5519a57e31798d598e32b2814c7ece
Davidvl <josuedavidvl18@gmail.com>
2023-08-01 02:46:17 -0600

    logica en javascript implementada

--- public/script.js
+++ public/script.js
@@ -1,6 +1,27 @@
 $(document).ready(() => {
+  $.get('/api/tareas', (data) => {
+    const listaTareas = $('#lista-tareas');
+    listaTareas.empty();
+    data.forEach((tarea) => {
+      listaTareas.append(`<li class="list-group-item">${tarea.descripcion}</li>`);
+    });
+  });
 
-    $.get('/api/tareas', (data) => {
+  $('#form-tarea').submit((event) => {
+    event.preventDefault();
+    const descripcion = $('#descripcion').val();
+    $.post('/api/tareas', { descripcion }, (data) => {
+
+        $('#lista-tareas').append(`<li class="list-group-item">${descripcion}</li>`);
+      $('#descripcion').val('');
+
+      $.get('/api/tareas', (data) => {
         const listaTareas = $('#lista-tareas');
-    } )
-} )
+        listaTareas.empty();
+        data.forEach((tarea) => {
+          listaTareas.append(`<li class="list-group-item">${tarea.descripcion}</li>`);
+        });
+      });
+    });
+  });
+});
